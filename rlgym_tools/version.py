12344e0e9a411151a8bf25349fad3c62cea1d863
Rolv-Arild <rolv_arild@hotmail.com>
2021-09-18 19:26:56 +0200

    Update version.py

--- rlgym_tools/version.py
+++ rlgym_tools/version.py
@@ -5,9 +5,13 @@
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
 
-__version__ = '1.1.4'
+__version__ = '1.1.5'
 
 release_notes = {
+    '1.1.5':
+    """
+    - Various bug fixes
+    """,
     '1.1.4':
     """
     - Bug fixes for AdvancedStacker
