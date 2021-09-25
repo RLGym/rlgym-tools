86a04b946e9452cc6461935c250efaef9a373181
Rolv-Arild <rolv_arild@hotmail.com>
2021-09-26 00:17:24 +0200

    Update sequential_rewards.py
    Fix indent

--- rlgym_tools/extra_rewards/sequential_rewards.py
+++ rlgym_tools/extra_rewards/sequential_rewards.py
@@ -2,11 +2,11 @@ from rlgym.utils.reward_functions import RewardFunction
 
 
 class SequentialRewards(RewardFunction):
-""" 
-A simple reward class that allows you to transition from one reward to the next at set intervals.
-Example: rewards, step_requirements = [reward1, reward2, reward3, etc], [10_000_000, 20_000_000, 30_000_000, etc]
-my_rewards = SequentialRewards(rewards, step_requirements)   
-"""
+    """ 
+    A simple reward class that allows you to transition from one reward to the next at set intervals.
+    Example: rewards, step_requirements = [reward1, reward2, reward3, etc], [10_000_000, 20_000_000, 30_000_000, etc]
+    my_rewards = SequentialRewards(rewards, step_requirements)   
+    """
     def __init__(self, rewards: list, steps: list):
         super().__init__()
         self.rewards_list = rewards
