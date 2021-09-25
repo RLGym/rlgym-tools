3b6cc5f1d3ee3e9de0b33667586fb6b31a46cbfc
Impossibum <86384743+Impossibum@users.noreply.github.com>
2021-09-25 06:16:31 -0400

    Create kickoff_reward.py
    A simple reward to help bots learn kickoffs

---
+++ rlgym_tools/extra_rewards/kickoff_reward.py
@@ -0,0 +1,21 @@
+from rlgym.utils.reward_functions import RewardFunction
+from rlgym.utils.reward_functions.common_rewards.player_ball_rewards import VelocityPlayerToBallReward
+
+"""
+a simple reward that encourages driving towards the ball fast while it's in the neutral kickoff position
+"""
+class KickoffReward(RewardFunction):
+    def __init__(self):
+        super().__init__()
+        self.vel_dir_reward = VelocityPlayerToBallReward()
+
+    def reset(self, initial_state: GameState):
+        self.vel_dir_reward.reset(initial_state)
+
+    def get_reward(
+        self, player: PlayerData, state: GameState, previous_action: np.ndarray
+    ) -> float:
+        reward = 0
+        if state.ball.position[0] == 0 and state.ball.position[1] == 0:
+            reward += self.vel_dir_reward.get_reward(player, state, previous_action)
+        return reward
