f248f8f829746b4e7687de643bbda5d86f0ca59c
Rolv-Arild <rolv_arild@hotmail.com>
2021-12-30 22:15:48 +0100

    Fix GameCondition

--- rlgym_tools/extra_terminals/game_condition.py
+++ rlgym_tools/extra_terminals/game_condition.py
@@ -4,32 +4,39 @@ from rlgym.utils.gamestates import GameState
 
 class GameCondition(TerminalCondition):  # Mimics a Rocket League game
     def __init__(self, tick_skip=8, seconds_left=300):
+        # NOTE: Since game isn't reset to kickoff by default,
+        # you need to keep this outside the main loop as well,
+        # checking the done variable after each GoalTerminal
         super().__init__()
         self.tick_skip = tick_skip
         self.timer = seconds_left
         self.overtime = False
+        self.done = False
         self.initial_state = None
 
     def reset(self, initial_state: GameState):
-        self.timer = 300 * 120 / self.tick_skip
-        self.initial_state = initial_state
+        if self.done:
+            self.timer = 300 * 120 / self.tick_skip
+            self.initial_state = initial_state
+            self.overtime = False
+            self.done = False
 
     def is_terminal(self, current_state: GameState) -> bool:
         differential = (current_state.blue_score - self.initial_state.blue_score) \
                        - (current_state.orange_score - self.initial_state.orange_score)
         if self.overtime:
             if differential != 0:
-                return True
+                self.done = True
             else:
                 self.timer += self.tick_skip / 120
-                return False
+                self.done = False
         else:
             if self.timer <= 0 and current_state.ball.position[3] <= 100:
                 # Can't detect ball on ground directly, should be an alright approximation
                 if differential != 0:
-                    return True
+                    self.done = True
                 else:
                     self.overtime = True
-                    return False
+                    self.done = False
         self.timer -= self.tick_skip / 120
-        return False
+        return self.done
