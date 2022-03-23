aa942b5a86d792471c26403ef1437ae1b2a57aad
Matej Novosad <40635376+mrkvicka22@users.noreply.github.com>
2022-03-23 16:21:19 +0100

    Fixed hoops setter for more players (#27)
    It now spawns the remaining players randomly on their halves.

--- rlgym_tools/extra_state_setters/hoops_setter.py
+++ rlgym_tools/extra_state_setters/hoops_setter.py
@@ -1,6 +1,6 @@
 from rlgym.utils.state_setters import StateSetter
 from rlgym.utils.state_setters import StateWrapper
-from rlgym.utils.common_values import BALL_RADIUS, CEILING_Z, BLUE_TEAM
+from rlgym.utils.common_values import BALL_RADIUS, CEILING_Z, BLUE_TEAM, ORANGE_TEAM
 import numpy as np
 from numpy import random as rand
 
@@ -50,24 +50,64 @@ class HoopsLikeSetter(StateSetter):
 
     def _reset_cars_random(self, state_wrapper: StateWrapper, spawn_radius: float):
         """
-        Function to set all cars inbetween the ball and net roughly facing the ball
+        Function to set all cars inbetween the ball and net roughly facing the ball. The other cars will be spawned
+        randomly.
 
         :param state_wrapper: StateWrapper object to be modified.
         :param spawn_radius: Float determining how far away to spawn the cars from the ball.
         """
+        orange_done = False
+        blue_done = False
         for i, car in enumerate(state_wrapper.cars):
-            # just shorthands for ball_x and ball_y
-            bx = state_wrapper.ball.position[0]
-            by = state_wrapper.ball.position[1]
-            car_spawn_radius = rand.triangular(0.8, 1, 1.2) * spawn_radius
-            if car.team_num == BLUE_TEAM:
+            if car.team_num == BLUE_TEAM and not blue_done:
+                # just shorthands for ball_x and ball_y
+                bx = state_wrapper.ball.position[0]
+                by = state_wrapper.ball.position[1]
+
+                # add small variation to spawn radius
+                car_spawn_radius = rand.triangular(0.8, 1, 1.2) * spawn_radius
+
                 # calculate distance from ball to goal.
                 R = ((BLUE_GOAL_POSITION[0] - bx) ** 2 + (BLUE_GOAL_POSITION[1] - by) ** 2) ** 0.5
 
                 # use similarity of triangles to calculate offsets
                 x_offset = ((BLUE_GOAL_POSITION[0] - bx) / R) * car_spawn_radius
                 y_offset = ((BLUE_GOAL_POSITION[1] - by) / R) * car_spawn_radius
-            else:
+
+                # offset the car's positions
+                car.set_pos(bx + x_offset, by + y_offset, 17)
+
+                # compute vector from ball to car
+                rel_ball_car_vector = car.position - state_wrapper.ball.position
+
+                # calculate the angle between the yaw vector and relative vector and use that as yaw.
+
+                yaw = np.arccos(np.dot(rel_ball_car_vector / np.linalg.norm(rel_ball_car_vector), self.yaw_vector))
+
+                # then sprinkle in more variation by offsetting by random angle up to pi/8 radians (this is arbitrary)
+                max_offset_angle = np.pi / 8
+                yaw += rand.random() * max_offset_angle * 2 - max_offset_angle
+
+                # random at least slightly above 0 boost amounts
+                car.boost = rand.uniform(0.2, 1)
+
+                # make car face the ball roughly
+                car.set_rot(pitch=0, roll=0, yaw=yaw)
+
+                # x angular velocity (affects pitch) set to 0
+                # y angular velocity (affects roll) set to 0
+                # z angular velocity (affects yaw) set to 0
+                car.set_ang_vel(x=0, y=0, z=0)
+                car.set_lin_vel(x=0, y=0, z=0)
+                blue_done = True
+            elif car.team_num == ORANGE_TEAM and not orange_done:
+                # just shorthands for ball_x and ball_y
+                bx = state_wrapper.ball.position[0]
+                by = state_wrapper.ball.position[1]
+
+                # add small variation to spawn radius
+                car_spawn_radius = rand.triangular(0.8, 1, 1.2) * spawn_radius
+
                 # calculate distance from ball to goal.
                 R = ((ORANGE_GOAL_POSITION[0] - bx) ** 2 + (ORANGE_GOAL_POSITION[1] - by) ** 2) ** 0.5
 
@@ -75,30 +115,42 @@ class HoopsLikeSetter(StateSetter):
                 x_offset = ((ORANGE_GOAL_POSITION[0] - bx) / R) * car_spawn_radius
                 y_offset = ((ORANGE_GOAL_POSITION[1] - by) / R) * car_spawn_radius
 
-            # offset the car's positions
-            car.set_pos(bx + x_offset, by + y_offset, 17)
+                # offset the car's positions
+                car.set_pos(bx + x_offset, by + y_offset, 17)
 
-            # compute vector from ball to car
-            rel_ball_car_vector = car.position - state_wrapper.ball.position
+                # compute vector from ball to car
+                rel_ball_car_vector = car.position - state_wrapper.ball.position
 
-            # calculate the angle between the yaw vector and relative vector and use that as yaw.
+                # calculate the angle between the yaw vector and relative vector and use that as yaw.
 
-            yaw = np.arccos(np.dot(rel_ball_car_vector / np.linalg.norm(rel_ball_car_vector), self.yaw_vector))
+                yaw = np.arccos(np.dot(rel_ball_car_vector / np.linalg.norm(rel_ball_car_vector), self.yaw_vector))
 
-            # then sprinkle in more variation by offsetting by random angle up to pi/8 radians (this is arbitrary)
-            max_offset_angle = np.pi / 8
-            yaw += rand.random() * max_offset_angle * 2 - max_offset_angle
+                # then sprinkle in more variation by offsetting by random angle up to pi/8 radians (this is arbitrary)
+                max_offset_angle = np.pi / 8
+                yaw += rand.random() * max_offset_angle * 2 - max_offset_angle
 
-            # random at least slightly above 0 boost amounts
-            car.boost = rand.uniform(0.2, 1)
+                # random at least slightly above 0 boost amounts
+                car.boost = rand.uniform(0.2, 1)
 
-            if car.team_num == BLUE_TEAM:
-                car.set_rot(pitch=0, roll=0, yaw=yaw)
-            else:
+                # make car face the ball roughly
                 car.set_rot(pitch=0, roll=0, yaw=yaw - np.pi if yaw > 0 else yaw + np.pi)
 
-            # x angular velocity (affects pitch) set to 0
-            # y angular velocity (affects roll) set to 0
-            # z angular velocity (affects yaw) set to 0
-            car.set_ang_vel(x=0, y=0, z=0)
-            car.set_lin_vel(x=0, y=0, z=0)
+                # x angular velocity (affects pitch) set to 0
+                # y angular velocity (affects roll) set to 0
+                # z angular velocity (affects yaw) set to 0
+                car.set_ang_vel(x=0, y=0, z=0)
+                car.set_lin_vel(x=0, y=0, z=0)
+                orange_done = True
+            else:
+                car.set_pos(rand.uniform(-4096,4096)),rand.uniform(0,-1**(car.team_num-1)*5120,17)
+
+                car.boost = rand.uniform(0.2, 1)
+
+                # make car face the ball roughly
+                car.set_rot(pitch=0, roll=0, yaw=rand.uniform(-np.pi, np.pi))
+
+                # x angular velocity (affects pitch) set to 0
+                # y angular velocity (affects roll) set to 0
+                # z angular velocity (affects yaw) set to 0
+                car.set_ang_vel(x=0, y=0, z=0)
+                car.set_lin_vel(x=0, y=0, z=0)
