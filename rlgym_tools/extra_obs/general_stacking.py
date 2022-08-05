c852c5193c2144fadb1fa61248e074891880e708
some-rando-rl <103870616+some-rando-rl@users.noreply.github.com>
2022-08-06 10:42:53 +1200

    Fixed stackers (#39)
    * Make GeneralStacker track stacks by car_id
    
    Also uses faster stacking method
    
    * Use faster stacking logic in AdvancedStacker

--- rlgym_tools/extra_obs/general_stacking.py
+++ rlgym_tools/extra_obs/general_stacking.py
@@ -1,8 +1,8 @@
-from collections import deque
 from typing import Any
 
 import numpy as np
 
+from rlgym.utils.common_values import NUM_ACTIONS
 from rlgym.utils.gamestates import PlayerData, GameState
 from rlgym.utils.obs_builders import ObsBuilder
 
@@ -19,26 +19,26 @@ class GeneralStacker(ObsBuilder):
     def __init__(self, obs: ObsBuilder, stack_size: int = 15):
         super().__init__()
         self.stack_size = stack_size
-        self.obs = obs
-        self.queue = deque([], maxlen=self.stack_size)
-        self.frame_n = 0
-        self.new = True
+        self.obs_builder = obs
 
     def reset(self, initial_state: GameState):
-        self.new = True
+        self.obs = {}
+        previous_action = np.zeros(NUM_ACTIONS)
+        for player in initial_state.players:
+            self.obs_builder.reset(initial_state=initial_state)
+            initial_obs = self.obs_builder.build_obs(player, initial_state, previous_action=previous_action)
+            self.obs[player.car_id] = np.concatenate([initial_obs] * self.stack_size)
+        self.obs_builder.reset(initial_state=initial_state)
 
     def build_obs(
             self, player: PlayerData, state: GameState, previous_action: np.ndarray
     ) -> Any:
-        # sort out the edge case after reset of environment
-        if self.new:
-            initial_obs = self.obs.build_obs(player, state, previous_action)
-            for _ in range(self.stack_size):
-                self.queue.appendleft(initial_obs)
-        self.new = False
+        frame = self.obs_builder.build_obs(player, state, previous_action)
 
-        frame = self.obs.build_obs(player, state, previous_action)
+        num = frame.shape[0]
+        obs = self.obs[player.car_id]
+        obs[num:] = obs[:-num]
+        obs[:num] = frame
 
-        self.frame_n += 1
-        self.queue.appendleft(frame)
-        return np.concatenate(self.queue)
+        return obs
+
