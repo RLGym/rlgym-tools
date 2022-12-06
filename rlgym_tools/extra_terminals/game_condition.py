86e312187e5971fc4f0d767841e1187454f3001e
Impossibum <86384743+Impossibum@users.noreply.github.com>
2022-12-06 15:01:57 -0500

    fixed finally?
    introduces a no touch timeout to GameCondition as well as patches a bug preventing proper resets

--- rlgym_tools/extra_terminals/game_condition.py
+++ rlgym_tools/extra_terminals/game_condition.py
@@ -1,9 +1,10 @@
 from rlgym.utils import TerminalCondition
 from rlgym.utils.gamestates import GameState
+from rlgym.utils.terminal_conditions.common_conditions import NoTouchTimeoutCondition
 
 
 class GameCondition(TerminalCondition):  # Mimics a Rocket League game
-    def __init__(self, tick_skip=8, seconds_left=300, forfeit_spg_limit=None, max_overtime=300):
+    def __init__(self, tick_skip=8, seconds_left=300, forfeit_spg_limit=None, max_overtime=300, max_no_touch = 60):
         # NOTE: Since game isn't reset to kickoff by default,
         # you need to keep this outside the main loop as well,
         # checking the done variable after each GoalTerminal
@@ -16,6 +17,7 @@ class GameCondition(TerminalCondition):  # Mimics a Rocket League game
         self.initial_state = None
         self.forfeit_spg_limit = forfeit_spg_limit  # SPG = Seconds Per Goal
         self.max_overtime = max_overtime
+        self.touch_timeout = NoTouchTimeoutCondition((120/tick_skip) * max_no_touch)
 
     def reset(self, initial_state: GameState):
         if self.done:
@@ -25,30 +27,38 @@ class GameCondition(TerminalCondition):  # Mimics a Rocket League game
             self.done = False
 
     def is_terminal(self, current_state: GameState) -> bool:
-        differential = (current_state.blue_score - self.initial_state.blue_score) \
-                       - (current_state.orange_score - self.initial_state.orange_score)
-        if self.overtime:
-            if differential != 0:
-                self.done = True
-            elif self.timer >= self.max_overtime:
-                self.done = True  # Call it a draw
-            else:
-                self.timer += self.tick_skip / 120
-                self.done = False
-        else:
-            if self.timer <= 0 and current_state.ball.position[2] <= 110:
-                # Can't detect ball on ground directly, should be an alright approximation.
-                # Anything below z vel of ~690uu/s should be detected. 50% for 1380 etc.
+        self.done = self.touch_timeout.is_terminal(current_state)
+        if not self.done:
+            differential = (current_state.blue_score - self.initial_state.blue_score) \
+                           - (current_state.orange_score - self.initial_state.orange_score)
+
+            if self.overtime:
                 if differential != 0:
                     self.done = True
+                elif self.timer >= self.max_overtime:
+                    self.done = True  # Call it a draw
                 else:
-                    self.overtime = True
+                    self.timer += self.tick_skip / 120
                     self.done = False
-            elif self.forfeit_spg_limit is not None \
-                    and abs(differential) >= 3 \
-                    and self.timer / abs(differential) < self.forfeit_spg_limit:
-                self.done = True
             else:
-                self.timer -= self.tick_skip / 120
+                if self.timer <= 0 and current_state.ball.position[2] <= 110:
+                    # Can't detect ball on ground directly, should be an alright approximation.
+                    # Anything below z vel of ~690uu/s should be detected. 50% for 1380 etc.
+                    if differential != 0:
+                        self.done = True
+                    else:
+                        self.overtime = True
+                        self.done = False
+                        return True
+                elif self.forfeit_spg_limit is not None \
+                        and abs(differential) >= 3 \
+                        and self.timer / abs(differential) < self.forfeit_spg_limit:
+                    self.done = True
+
+                elif self.timer <= 0:
+                    self.done = True
+
+                else:
+                    self.timer -= self.tick_skip / 120
 
         return self.done
