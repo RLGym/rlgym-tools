5ef9b5d54ad89e333bb229dff368a24d87629223
Matthew Allen <AechPro@users.noreply.github.com>
2023-01-12 13:56:06 -0500

    Update rllib_example.py
    Updated to use RAINBOW

--- rlgym_tools/examples/rllib_example.py
+++ rlgym_tools/examples/rllib_example.py
@@ -1,91 +1,47 @@
-import logging
-import os
+from ray.rllib.algorithms.apex_dqn import ApexDQNConfig
+from test_marl_env import RLLibEnv
+from ray.tune import register_env
+from nexto_action_space import NectoActionParser
 
-import numpy as np
-import ray
-from gym.spaces import Box, Discrete
-from ray.rllib.agents.ppo import PPOTorchPolicy, PPOTrainer, APPOTrainer
-from ray.rllib.examples.env.multi_agent import MultiAgentCartPole
-from ray.tune import register_env, tune
-from ray.tune.logger import pretty_print
+def make_env():
+    import rlgym
+    team_size = 1
+    self_play = True
+    n_agents = team_size*2 if self_play else team_size
 
-import rlgym
-from rlgym.utils.obs_builders import AdvancedObs
-from rlgym.utils.reward_functions.common_rewards import VelocityPlayerToBallReward
-from rlgym_tools.rllib_utils import RLLibEnv
+    env = rlgym.make(action_parser=NectoActionParser(n_agents),
+                     team_size=team_size,
+                     spawn_opponents=self_play,
+                     use_injector=True)
 
-if __name__ == '__main__':
-    ray.init(address='auto', _redis_password='5241590000000000', logging_level=logging.DEBUG)
-
-
-    def create_env(env_config):
-        return RLLibEnv(rlgym.make(self_play=True,
-                                   obs_builder=AdvancedObs(),
-                                   reward_fn=VelocityPlayerToBallReward()))
+    return RLLibEnv(env)
 
 
-    register_env("RLGym", create_env)
+if __name__ == "__main__":
+    register_env("rlgym", lambda c: make_env())
 
-    policy = PPOTorchPolicy, Box(-np.inf, np.inf, (107,)), Box(-1.0, 1.0, (8,)), {}
-    # policy = PPOTorchPolicy, Box(-np.inf, np.inf, (4,)), Discrete(2), {}
+    cfg = ApexDQNConfig()
+    cfg.framework("torch")
+    cfg.rollouts(num_rollout_workers=2, )
+    cfg.resources(num_gpus=1)
+    cfg.environment(env="rlgym")
+    cfg.num_atoms = 51
 
-    ppo_trainer = PPOTrainer(
-        env="RLGym",  # "CartPole-v0",
-        config={
-            "multiagent": {
-                "policies": {"ppo_policy": policy},
-                "policy_mapping_fn": (lambda agent_id, **kwargs: "ppo_policy"),
-                "policies_to_train": ["ppo_policy"],
-            },
-            "env_config": {
-                "num_agents": 2
-            },
-            "model": {
-                "vf_share_layers": True,
-            },
-            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-            "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
-            "num_workers": 0,
-            "num_cpus_per_worker": 0,
-            # "num_envs_per_worker": 2,
-            # "remote_worker_envs": True,
-            # "sample_async": True,
-            "framework": "torch"
-        }
-    )
+    algo = cfg.build()
 
-    for i in range(1000000):
-        print("== Iteration", i, "==")
+    for i in range(100000000):
+        result = algo.train()
+        print("Epoch: {}\n"
+              "Mean Reward: {:7.3f}\n"
+              "Mean Ep Len: {:7.2f}\n"
+              "Timesteps: {}\n"
+              "Time elapsed: {:7.2f}\n"
+              .format(result["training_iteration"],
+                      result["episode_reward_mean"],
+                      result["episode_len_mean"],
+                      result["timesteps_total"],
+                      result["time_total_s"]))
 
-        # improve the PPO policy
-        print("-- PPO --")
-        result_ppo = ppo_trainer.train()
-        print(pretty_print(result_ppo))
-    print("Done training")
-
-    # # This does not work for some reason
-    # tune.run(
-    #     "PPO",
-    #     config={
-    #         "env": MultiAgentCartPole,
-    #         "env_config": {
-    #             "num_agents": 2
-    #         },
-    #         "multiagent": {
-    #             "policies": {"ppo_policy": policy},
-    #             "policy_mapping_fn": (lambda agent_id, **kwargs: "ppo_policy"),
-    #             "policies_to_train": ["ppo_policy"],
-    #         },
-    #         "model": {
-    #             "vf_share_layers": True,
-    #         },
-    #         # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-    #         "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
-    #         "num_workers": 2,
-    #         "num_cpus_per_worker": 1,
-    #         "num_workers_per_env": 2,
-    #         "remote_worker_envs": True,
-    #         "sample_async": True,
-    #         "framework": "torch"
-    #     }
-    # )
+        if i % 5 == 0:
+            checkpoint_dir = algo.save()
+            print(f"Checkpoint saved in directory {checkpoint_dir}")
