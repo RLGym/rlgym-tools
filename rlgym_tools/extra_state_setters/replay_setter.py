dca9e5a9c457779de674939c4bdab1db8320e6d0
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2023-06-07 10:01:58 -0400

    new functions on replay_setter.py to expand or shrink cars in replay data to match the state_wrapper. Controlled by a boolean parameter expand_shrink_cars on ReplaySetter.

--- rlgym_tools/extra_state_setters/replay_setter.py
+++ rlgym_tools/extra_state_setters/replay_setter.py
@@ -6,15 +6,66 @@ from rlgym.utils.state_setters import StateSetter
 from rlgym.utils.state_setters import StateWrapper
 
 
+def _expand_cars(state_wrapper: StateWrapper, data: np.ndarray):
+    rng = np.random.default_rng()
+    num_cars_in_replay = (len(data) - 9) // 13
+    num_cars_to_add = len(state_wrapper.cars) - num_cars_in_replay
+    car_data = np.split(data[9:], num_cars_in_replay)
+    item_positions = [data[:3]]
+    for i, car in car_data:
+        item_positions.append(car_data[i][:3])
+    # longest dimension of any hitbox is 131.49 (breakout) so check 150 distance for safety
+    for i in range(num_cars_to_add):
+        checks = 0
+        closest_dist = 0
+        x = 0
+        y = 0
+        z = 17
+        while closest_dist <= 150:
+            x = rng.uniform(-2800, 2800)
+            y = rng.uniform(-3800, 3800)
+            item_positions.sort(key=lambda p: np.linalg.norm(p - np.array([x, y, z])))
+            closest_dist = np.linalg.norm(item_positions[0] - np.array([x, y, z]))
+            checks += 1
+            # having trouble finding space on the floor, put it in the air
+            # this seems extremely rare. I ran 100_000 tests with 6 initial cars and ball at 17
+            # and the most checks ever was 4
+            if checks > 5:
+                z = rng.uniform(140, 1800)
+        item_positions.append(np.array([x, y, z]))
+        data = np.append(data, [x, y, z, 0, rng.uniform(-np.pi, np.pi), 0, rng.uniform(0, 500), rng.uniform(0, 500),
+                                0, 0, 0, 0, rng.uniform(0, 1)])
+
+    return data
+
+
+def _shrink_cars(state_wrapper: StateWrapper, data: np.ndarray):
+    num_cars_in_replay = (len(data) - 9) // 13
+    num_cars_to_remove = num_cars_in_replay - len(state_wrapper.cars)
+    per_team_to_remove = num_cars_to_remove // 2
+    leftover = num_cars_to_remove - (per_team_to_remove * 2)
+    blue_end = (num_cars_in_replay // 2) - 1
+    orange_end = (num_cars_in_replay - 1)
+    to_delete = [*range(blue_end - per_team_to_remove + 1, blue_end + 1)]
+    to_delete.extend(range(orange_end - per_team_to_remove + 1 - leftover, orange_end + 1))
+    car_data = np.split(data[9:], num_cars_in_replay)
+    car_data = np.delete(car_data, to_delete, axis=0)
+    car_data = car_data.flatten()
+    ball_data = data[:9]
+    return np.concatenate((ball_data, car_data))
+
+
 class ReplaySetter(StateSetter):
-    def __init__(self, ndarray_or_file: Union[str, np.ndarray]):
+    def __init__(self, ndarray_or_file: Union[str, np.ndarray], expand_shrink_cars=False):
         """
         ReplayBasedSetter constructor
 
         :param ndarray_or_file: A file string or a numpy ndarray of states for a single game mode.
+        :param expand_shrink_cars: an integer number of cars to expect, to expand or contract a setter which isn't the right size
         """
         super().__init__()
 
+        self.expand_shrink_cars = expand_shrink_cars
         if isinstance(ndarray_or_file, np.ndarray):
             self.states = ndarray_or_file
         elif isinstance(ndarray_or_file, str):
@@ -80,7 +131,12 @@ class ReplaySetter(StateSetter):
         """
 
         data = self.states[np.random.choice(len(self.states), p=self.probabilities)]
-        assert len(data) == len(state_wrapper.cars) * 13 + 9, "Data given does not match current game mode"
+        if not self.expand_shrink_cars:
+            assert len(data) == len(state_wrapper.cars) * 13 + 9, "Data given does not match current game mode"
+        elif len(data) < len(state_wrapper.cars) * 13 + 9:
+            data = _expand_cars(state_wrapper, data)
+        else:
+            data = _shrink_cars(state_wrapper, data)
         self._set_ball(state_wrapper, data)
         self._set_cars(state_wrapper, data)
 
@@ -110,3 +166,5 @@ class ReplaySetter(StateSetter):
         state_wrapper.ball.set_pos(*data[:3])
         state_wrapper.ball.set_lin_vel(*data[3:6])
         state_wrapper.ball.set_ang_vel(*data[6:9])
+
+
