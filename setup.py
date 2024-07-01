35d02c636b383b532c8d5602676c8bbd1aef9bf7
Hazel Hulet <7584038+CosmicHazel@users.noreply.github.com>
2024-07-01 04:21:26 -0700

    Fixes failed pip setup by allowing RLGym release candidates (#54)
    * fixed setup to get rlgym v2
    
    * missing comma

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author='Rolv-Arild Braaten, Lucas Emery and Matthew Allen',
     url='https://rlgym.github.io',
     install_requires=[
-        'rlgym>=2.0.0',
+    'rlgym>=2.0.0rc0',  # Including release candidates starting from 2.0.0
     ],
     python_requires='>=3.9',
     license='Apache 2.0',
