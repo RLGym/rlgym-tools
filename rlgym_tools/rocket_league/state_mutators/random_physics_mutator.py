Fix RandomPhysicsMutator giving invalid rotation and disabling ball collision

rlgym_tools/rocket_league/state_mutators/random_physics_mutator.py
@@ -4,7 +4,7 @@ import numpy as np
 from rlgym.api import StateMutator
 from rlgym.rocket_league.api import GameState
 from rlgym.rocket_league.common_values import SIDE_WALL_X, BACK_WALL_Y, CEILING_Z
-from rlgym.rocket_league.math import rand_vec3, rand_uvec3
+from rlgym.rocket_league.math import rand_vec3, rand_uvec3, normalize
 
 from rlgym_tools.rocket_league.reward_functions.aerial_distance_reward import RAMP_HEIGHT
 
@@ -12,6 +12,7 @@ from rlgym_tools.rocket_league.reward_functions.aerial_distance_reward import RA
 class RandomPhysicsMutator(StateMutator[GameState]):
     def apply(self, state: GameState, shared_info: Dict[str, Any]) -> None:
         padding = 100  # Ball radius and car hitbox with biggest radius are both below this
+        i = 0
         for po in [state.ball] + [car.physics for car in state.cars.values()]:
             while True:
                 new_pos = np.random.uniform([-SIDE_WALL_X + padding, -BACK_WALL_Y + padding, 0 + padding],
@@ -31,7 +32,13 @@ class RandomPhysicsMutator(StateMutator[GameState]):
             po.position = new_pos
             po.linear_velocity = rand_vec3(2300)
             po.angular_velocity = rand_vec3(5)
-            fw = rand_uvec3()
-            up = rand_uvec3()
-            right = np.cross(up, fw)  # Technically not correct but testing shows that this is what's used
-            po.rotation_mtx = np.stack([fw, right, up])
+            if i > 0:
+                # We don't need to set rotation for the ball
+                fw = rand_uvec3()
+                up = rand_uvec3()
+                right = normalize(np.cross(up, fw))
+                up = normalize(np.cross(fw, right))
+                rot_mat = np.stack([fw, right, up])
+                # assert np.allclose(np.linalg.norm(rot_mat, axis=1), 1)
+                po.rotation_mtx = rot_mat
+            i += 1