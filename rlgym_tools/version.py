5959713867b41c6e761d9f57d618cff10b6800bb
Rolv-Arild <rolv_arild@hotmail.com>
2025-04-11 15:10:17 +0200

    Update version.py

--- rlgym_tools/version.py
+++ rlgym_tools/version.py
@@ -5,10 +5,13 @@
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
 
-__version__ = '2.2.4'
+__version__ = '2.2.5'
 
 
 release_notes = {
+    '2.2.5': """
+    - Add new carball.exe
+    """,
     '2.2.4': """
     - Fix dodge torque adjustment by normalizing torque in replay converter.
     """,
