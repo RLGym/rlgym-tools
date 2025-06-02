f2516a74832242295f568ec4b49fa824301619f8
Rolv-Arild <rolv_arild@hotmail.com>
2025-06-02 13:57:14 +0200

    Update version.py

--- rlgym_tools/version.py
+++ rlgym_tools/version.py
@@ -5,10 +5,13 @@
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
 
-__version__ = '2.3.1'
+__version__ = '2.3.2'
 
 
 release_notes = {
+    '2.3.2': """
+    - In pick_action.py: better logic for when jump matters, and fix for throttle logic when no boost.
+    """,
     '2.3.1': """
     - Fix flip reset obtain detection in FlipResetReward
     """,
