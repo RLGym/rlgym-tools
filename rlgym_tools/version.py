a3043ba48b4233a55523903fa549731ada1f61bb
Rolv-Arild <rolv_arild@hotmail.com>
2026-04-10 22:25:10 +0200

    Update version.py

--- rlgym_tools/version.py
+++ rlgym_tools/version.py
@@ -5,9 +5,12 @@
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
 
-__version__ = '2.6.3'
+__version__ = '2.6.4'
 
-release_notes = {
+release_notes = {   
+    '2.6.4': """
+    - Fix check for big boost pad in replay to RLGym converter
+    """,
     '2.6.3': """
     - Make normalization safe from division by 0 in dodge_relative_rot_mtx
     """,
